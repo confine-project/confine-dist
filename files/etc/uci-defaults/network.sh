10c2df7e096b7c9567a989d698e5e530efaf758b
xavileon <xleon@ac.upc.edu>
2012-02-23 19:15:52 +0100

    Solved the way eth0 and eth1 are linked to wan and lan bridges.

--- files/etc/uci-defaults/network.sh
+++ files/etc/uci-defaults/network.sh
@@ -2,14 +2,14 @@
 
 echo "Configuring network for CONFINE..."
 uci set network.wan=interface
-uci set network.wan.ifname=eth1
+uci set network.wan.type=bridge
+uci set network.wan.ifname=eth0
 uci set network.wan.proto=dhcp
 
 uci set network.lan=interface
-uci set network.lan.ifname=eth0
-uci set network.lan.proto=static
-uci set network.lan.ipaddr=192.168.0.1
-uci set network.lan.netmask=255.255.255.0
+uci set network.lan.type=bridge
+uci set network.lan.ifname=eth1
+uci set network.lan.proto=dhcp
 
 uci commit
 
