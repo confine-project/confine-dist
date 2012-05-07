fafbd297b62a513397939344f00f97a6cca1c105
Axel Neumann <neumann@cgws.de>
2012-05-07 14:13:15 +0200

    statically add ipv6 recovery address

--- files/etc/uci-defaults/network.sh
+++ files/etc/uci-defaults/network.sh
@@ -14,11 +14,9 @@ uci set network.local.macaddr=$( ip link show dev eth0 | grep "link/ether" | awk
 uci set network.recovery=alias
 uci set network.recovery.interface=local
 uci set network.recovery.proto=static
-uci set network.recovery.ipaddr=192.168.241.130  # see VCT_RD_RESCUE_V4_IP
-uci set network.recovery.netmask=255.255.255.128 # see VCT_RD_RESCUE_V4_PL
+uci set network.recovery.ip6addr=fdbd:e804:6aa9:ffff::2/64 # see VCT_RD_RESCUE_V6_IP
+uci set network.recovery.ipaddr=192.168.241.130            # see VCT_RD_RESCUE_V4_IP
+uci set network.recovery.netmask=255.255.255.128           # see VCT_RD_RESCUE_V4_PL
 
 uci commit
-
-mkdir -p /etc/uci-originals
-cp /etc/uci-defaults/network.sh /etc/uci-originals/
 
