eaea4adcca72cd2e48b57f0ea4c72fc0ce85f294
Axel Neumann <neumann@cgws.de>
2012-07-20 07:01:49 +0200

    confine: use netifd configuratin style for alias IPs (old works but unstable)

--- files/etc/uci-defaults/confine-network.sh
+++ files/etc/uci-defaults/confine-network.sh
@@ -12,15 +12,15 @@ uci set network.local.proto=dhcp
 uci set network.local.ifname=eth0
 #uci set network.local.macaddr=$( ip link show dev eth0 | grep "link/ether" | awk '{print $2}' )
 
-uci set network.recovery1=alias
-uci set network.recovery1.interface=local
+uci set network.recovery1=interface
+uci set network.recovery1.ifname=br-local
 uci set network.recovery1.proto=static
 uci set network.recovery1.ip6addr=fdbd:e804:6aa9:0001::2/64
 uci set network.recovery1.ipaddr=192.168.241.130
 uci set network.recovery1.netmask=255.255.255.128
 
-uci set network.recovery2=alias
-uci set network.recovery2.interface=local
+uci set network.recovery2=interface
+uci set network.recovery2.ifname=br-local
 uci set network.recovery2.proto=static
 uci set network.recovery2.ip6addr=fdbd:e804:6aa9:0002:$( confine.lib eui64_from_link eth0 soft )/64
 
