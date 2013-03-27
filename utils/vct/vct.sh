39570cba8f702a50d9384b0dfd0a2697e59f04ac
Axel Neumann <neumann@cgws.de>
2013-03-27 09:44:04 +0100

    vct: simplify vct_system_purge_server

--- utils/vct/vct.sh
+++ utils/vct/vct.sh
@@ -474,10 +474,10 @@ vct_system_install_server() {
 
 vct_system_purge_server() {
 	vct_sudo python server/manage.py stopservices --no-postgresql  || true
-	ps aux | grep ^postgres > /dev/null || vct_sudo /etc/init.d/postgresql start || true
-	sudo su postgres -c 'psql -c "DROP DATABASE controller;"'  || true
-	grep "^confine" /etc/passwd > /dev/null && vct_sudo deluser --force --remove-home confine  || true
-	grep "^confine" /etc/group  > /dev/null && vct_sudo delgroup confine  || true
+	ps aux | grep ^postgres > /dev/null || vct_sudo /etc/init.d/postgresql start # || true
+	sudo su postgres -c 'psql -c "DROP DATABASE controller;"'  # || true
+	#grep "^confine" /etc/passwd > /dev/null && vct_sudo deluser --force --remove-home confine  || true
+	#grep "^confine" /etc/group  > /dev/null && vct_sudo delgroup confine  || true
 	if [ -d $VCT_SERVER_DIR ]; then
 	    vct_do rm -rf $VCT_SERVER_DIR  || true
 	fi
@@ -700,12 +700,11 @@ EOF
     fi
 
 
-    if [ $CMD_INSTALL ] && [ -d $VCT_SERVER_DIR ] && ( ! [ "$VCT_SERVER" = "y" ] ||  [ $UPD_SERVER ] ); then
+    if [ $CMD_INSTALL ] && ( ! [ "$VCT_SERVER" = "y" ] ||  [ $UPD_SERVER ] ); then
 	echo "" >&2
-	echo "Purge server installation?" >&2
-	read -p "Please type 'purge' or anything else to skip: " QUERY >&2
+	read -p "Purge server installation (type 'y' or anything else to skip): " QUERY >&2
 
-	if [ "$QUERY" == "purge" ] ; then
+	if [ "$QUERY" == "y" ] ; then
 	    vct_system_purge_server
 	fi
     fi
