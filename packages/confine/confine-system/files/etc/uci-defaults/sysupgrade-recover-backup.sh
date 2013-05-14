265bf4ea36da7641af6b56863c1d7a0398b298a0
Daniel Martí <mvdan@mvdan.cc>
2013-05-14 17:54:33 +0200

    Add uci-defaults script

---
+++ packages/confine/confine-system/files/etc/uci-defaults/sysupgrade-recover-backup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p /tmp/boot
+mount -t ext2 -o rw,noatime /dev/sda1 /tmp/boot
+
+if [ -f /tmp/boot/fstaB ]; then
+    mv /tmp/boot/fstab /etc/config/fstab
+    /etc/init.d/fstab start
+    reboot -f
+elif [ -f /tmp/boot/conf.tgz ]; then
+    tar -C / -xzf /tmp/boot/conf.tgz
+    rm /tmp/boot/conf.tgz
+    reboot -f
+fi
+
+umount -l /tmp/boot
+rmdir /tmp/boot
+
+exit 0
