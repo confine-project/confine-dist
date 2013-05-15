6a85dae8898c8c8107e383ea4eec2efcc5992fe6
Daniel Martí <mvdan@mvdan.cc>
2013-05-15 17:22:31 +0200

    Small fixes to confine.sysupgrade's uci script

--- packages/confine/confine-system/files/etc/uci-defaults/sysupgrade-recover-backup.sh
+++ packages/confine/confine-system/files/etc/uci-defaults/sysupgrade-recover-backup.sh
@@ -5,14 +5,20 @@ mount -t ext2 -o rw,noatime /dev/sda1 /tmp/boot
 
 if [ -f /tmp/boot/fstab ]; then
     mv /tmp/boot/fstab /etc/config/fstab
-    /etc/init.d/fstab start
+    mkdir /overlay /home
+    /etc/init.d/fstab enable
+    mount -o remount,ro /tmp/boot
+    umount -l /tmp/boot
     reboot -f
 elif [ -f /tmp/boot/conf.tgz ]; then
     tar -C / -xzf /tmp/boot/conf.tgz
     rm /tmp/boot/conf.tgz
+    mount -o remount,ro /tmp/boot
+    umount -l /tmp/boot
     reboot -f
 fi
 
+mount -o remount,ro /tmp/boot
 umount -l /tmp/boot
 rmdir /tmp/boot
 
