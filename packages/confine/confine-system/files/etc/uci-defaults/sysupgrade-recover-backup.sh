18b7ba80a8ae87533629ca4f89d19ad2a4eca558
Daniel Martí <mvdan@mvdan.cc>
2013-05-21 18:36:29 +0200

    Small changes to sysupgrade

--- packages/confine/confine-system/files/etc/uci-defaults/sysupgrade-recover-backup.sh
+++ packages/confine/confine-system/files/etc/uci-defaults/sysupgrade-recover-backup.sh
@@ -4,18 +4,18 @@ mkdir -p /tmp/boot
 mount -t ext2 -o rw,noatime /dev/sda1 /tmp/boot
 
 if [ -f /tmp/boot/fstab ]; then
-    mv /tmp/boot/fstab /etc/config/fstab
-    mkdir /overlay /home
+    mv -f /tmp/boot/fstab /etc/config/fstab
+    mkdir -p /overlay /home
     /etc/init.d/fstab enable
     mount -o remount,ro /tmp/boot
     umount -l /tmp/boot
-    reboot -f
+    reboot
 elif [ -f /tmp/boot/conf.tgz ]; then
     tar -C / -xzf /tmp/boot/conf.tgz
     rm /tmp/boot/conf.tgz
     mount -o remount,ro /tmp/boot
     umount -l /tmp/boot
-    reboot -f
+    reboot
 fi
 
 mount -o remount,ro /tmp/boot
