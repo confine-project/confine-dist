fcb75f619f89b8eb289437ea41d577ab7cc76602
glic3rinu <vct@vct.(none)>
2013-07-22 10:51:05 +0000

    Updated controller to 0.9.2a8

--- utils/vct/server/vct/admin.py
+++ utils/vct/server/vct/admin.py
@@ -4,8 +4,13 @@ from django import forms
 from django.core import validators
 from django.core.exceptions import ValidationError
 
+from controller.admin.utils import insert_change_view_action, insert_action, get_modeladmin
 from controller.models.utils import get_file_field_base_path
 from controller.utils import is_installed
+from nodes.models import Node
+from slices.admin import TemplateAdmin, SliceAdmin, SliceSliversAdmin
+from slices.models import Template, Sliver, Slice
+from .actions import vct
 
 
 class LocalFileField(forms.fields.FileField):
@@ -28,7 +33,7 @@ def local_files_form_factory(model_class, field_name, base_class=forms.ModelForm
             choices = (('empty', '---------'),) + choices
             self.fields[field_name].required = False
         self.fields[field_name].widget = forms.widgets.Select(choices=choices)
-
+    
     def clean_field(self):
         value = self.cleaned_data.get(field_name)
         return value if value != 'empty' else ''
@@ -42,12 +47,22 @@ if is_installed('firmware'):
     from firmware.admin import BaseImageInline
     from firmware.models import BaseImage
     BaseImageInline.form = local_files_form_factory(BaseImage, 'image')
+    
+    # Replace node firmware download for "VM manager"
+    insert_change_view_action(Node, vct)
+    insert_action(Node, vct)
+    node_modeladmin = get_modeladmin(Node)
+    old_get_change_view_actions_as_class = node_modeladmin.get_change_view_actions_as_class
+    def get_change_view_actions_as_class(self):
+        actions = old_get_change_view_actions_as_class()
+        return [ action for action in actions if action.url_name != 'firmware' ]
+    type(node_modeladmin).get_change_view_actions_as_class = get_change_view_actions_as_class
 
 
-if is_installed('slices'):
-    from slices.admin import TemplateAdmin, SliceAdmin, SliceSliversAdmin
-    from slices.models import Template, Sliver, Slice
-    TemplateAdmin.form = local_files_form_factory(Template, 'image')
-    SliceAdmin.form = local_files_form_factory(Slice, 'exp_data', base_class=SliceAdmin.form)
-    SliceSliversAdmin.form = local_files_form_factory(Sliver, 'exp_data')
+# Slices customization
+TemplateAdmin.form = local_files_form_factory(Template, 'image')
+SliceAdmin.form = local_files_form_factory(Slice, 'exp_data', base_class=SliceAdmin.form)
+SliceSliversAdmin.form = local_files_form_factory(Sliver, 'exp_data')
+
+
 
