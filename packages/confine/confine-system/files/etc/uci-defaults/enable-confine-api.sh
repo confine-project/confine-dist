dc5c2986603910d365cac00119d69230c38b0c14
glic3rinu <vct@vct.(none)>
2013-07-22 13:09:22 +0000

    Removed FIXME comment on enable-confine-api.sh

--- packages/confine/confine-system/files/etc/uci-defaults/enable-confine-api.sh
+++ packages/confine/confine-system/files/etc/uci-defaults/enable-confine-api.sh
@@ -2,7 +2,6 @@
 
 echo "Enable confine CGI API"
 
-# FIXME adding this config prevents uhttpd from automatically starting during boot :(
 uci set uhttpd.main.lua_prefix=/confine/api
 uci set uhttpd.main.lua_handler=/usr/lib/lua/confine/api.lua
 
