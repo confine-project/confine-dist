66cd97036cc5972d5f2230d93c9674ba00c8ff51
Marc Aymerich <vct@vct.(none)>
2013-11-25 13:36:55 +0000

    vct/server: make debug.TECHNICAL_500_TEMPLATE more verbose and less dependent of futures

--- utils/vct/server/server/__init__.py
+++ utils/vct/server/server/__init__.py
@@ -1,3 +1,3 @@
 from django.views import debug
-debug.TECHNICAL_500_TEMPLATE = """<!--{% load firstof from future %}{% firstof exception_type 'Report' %}{% if request %} at {{ request.path_info }}{% endif %}:
-{% firstof exception_value 'No exception message supplied' %}-->"""+debug.TECHNICAL_500_TEMPLATE
+debug.TECHNICAL_500_TEMPLATE = """<!--{{ exception_type }}{% if request %} at {{ request.path_info }}{% endif %}:
+{{ exception_value }} {{ lastframe.filename|escape }} in {{ lastframe.function|escape }}, line {{ lastframe.lineno }}-->"""+debug.TECHNICAL_500_TEMPLATE
