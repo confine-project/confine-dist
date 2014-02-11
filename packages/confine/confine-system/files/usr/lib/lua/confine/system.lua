4dcbe8a34c8185a9f449ceb11af1faf2f65e130e
Axel Neumann <neumann@cgws.de>
2014-02-11 09:08:47 +0000

    [CNS] allow reproducing lost tinc conf (http://redmine.confine-project.eu/issues/342)

--- packages/confine/confine-system/files/usr/lib/lua/confine/system.lua
+++ packages/confine/confine-system/files/usr/lib/lua/confine/system.lua
@@ -218,10 +218,11 @@ function get_system_conf(sys_conf, arg)
 	conf.node_base_path        = conf.node_base_path or flags["node-base-path"] or uci.get("confine", "node", "base_path") or NODE_BASE_PATH
 	assert(conf.node_base_path:match("/api$"), "node-base-path MUST end with /api")
 	conf.node_base_uri         = "http://["..conf.mgmt_ipv6_prefix48..":".."%X"%conf.id.."::2]"..conf.node_base_path
---	conf.server_base_uri       = "https://controller.confine-project.eu/api"
 	conf.server_base_path      = conf.server_base_path or flags["server-base-path"] or uci.get("confine", "server", "base_path") or SERVER_BASE_PATH
 	assert(conf.server_base_path:match("/api$"), "server-base-path MUST end with /api")
-	conf.server_base_uri       = lsys.getenv("SERVER_URI") or "https://["..conf.mgmt_ipv6_prefix48.."::2]"..conf.server_base_path
+	conf.server_tcp_proto      = uci.get("confine", "server", "tcp_proto") or "https"
+	assert(conf.server_tcp_proto == "http" or conf.server_tcp_proto == "https", "optional server.tcp_proto MUST be http or https")
+	conf.server_base_uri       = conf.server_tcp_proto .. "://[" .. conf.mgmt_ipv6_prefix48 .. "::2]" .. conf.server_base_path
 	
 	conf.sys_state             = uci.get("confine", "node", "state")
 	conf.boot_sn               = tonumber(uci.get("confine", "node", "boot_sn", 0))
