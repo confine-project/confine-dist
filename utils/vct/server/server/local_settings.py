e8414efd0afb5f5d7c9d1d477e2e86f29936aaba
Axel Neumann <neumann@cgws.de>
2014-02-13 09:40:33 +0000

    [vct] use VCT_SERVER_VERSION=0.10.2 and reenable controller monitoring app

--- utils/vct/server/server/local_settings.py
+++
@@ -1,4 +0,0 @@
-from controller.utils.apps import add_app, remove_app
-from settings import *
-
-INSTALLED_APPS = remove_app(INSTALLED_APPS, 'monitor')                                                                    
