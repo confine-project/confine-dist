65e7a7d4a1547c8e061f9cf1f52dff6531665763
Axel Neumann <neumann@cgws.de>
2015-01-03 14:46:09 +0000

    [CNS] add password warning (#125)

--- files/etc/uci-defaults/confine-passwd.sh
+++ files/etc/uci-defaults/confine-passwd.sh
@@ -8,3 +8,22 @@ confine
 EOF
 
 
+pw_warn_file=/root/.password-warning
+
+cat >> "/etc/profile" <<EOF
+
+# Show a reminder to set a proper password.
+if [ -f "$pw_warn_file" ]; then
+        cat $pw_warn_file
+fi
+EOF
+
+cat >> "/root/.password-warning" <<EOF
+
+**WARNING:**
+
+Please remember to change root's password using ``passwd root``.
+Then you may remove ``$pw_warn_file``
+to suppress this warning.
+
+EOF
