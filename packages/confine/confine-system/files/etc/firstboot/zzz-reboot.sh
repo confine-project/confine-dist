7aeb263bac40b2bbd2b5f0abf8713624b9eb4d9e
Axel Neumann <neumann@cgws.de>
2015-01-07 08:57:46 +0000

    [CNS] log firstboot event to /root/confine.daemon-starts

--- packages/confine/confine-system/files/etc/firstboot/zzz-reboot.sh
+++ packages/confine/confine-system/files/etc/firstboot/zzz-reboot.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 echo "Firstboot executed, rebooting system in 5 seconds"
 sleep 5
+echo "$(date) zzz-reboot" >> /root/confine.daemon-starts
 reboot
+echo "$(date) zzz-reboot-000" >> /root/confine.daemon-starts
+sleep 5
+echo "$(date) zzz-reboot-005" >> /root/confine.daemon-starts
+sleep 5
+echo "$(date) zzz-reboot-010" >> /root/confine.daemon-starts
+sleep 100
+echo "$(date) zzz-reboot-110" >> /root/confine.daemon-starts
