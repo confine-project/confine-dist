64dbe897488dd5ee999d1b4684491b90b6bc97ce
Axel Neumann <neumann@cgws.de>
2015-02-18 09:24:14 +0000

    [CNS] merge firstboot scripts enable-uhttpd-ipv6.sh into enable-confine-api.sh and restart uhttpd

--- packages/confine/confine-system/files/etc/firstboot/enable-confine-api.sh
+++ packages/confine/confine-system/files/etc/firstboot/enable-confine-api.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+echo "Enable IPv6 for uhttpd"
+
+cp /etc/config/uhttpd /etc/config/uhttpd.orig
+
+uci set uhttpd.main.listen_http='0.0.0.0:80 [::]:80'
+uci set uhttpd.main.listen_https='0.0.0.0:443 [::]:443'
+
 echo "Enable confine CGI API"
 
 uci set uhttpd.main.lua_prefix=/confine/api
 uci set uhttpd.main.lua_handler=/usr/lib/lua/confine/api.lua
 
 uci commit uhttpd
+
+/etc/init.d/uhttpd restart
