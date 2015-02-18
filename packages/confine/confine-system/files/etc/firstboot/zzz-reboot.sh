41c479d90c0e3c0e95e6e94de02dfc1d92d0e7c0
Axel Neumann <neumann@cgws.de>
2015-02-18 11:56:30 +0000

    Revert "[CNS] sync and reset -f (hard) during /etc/firstboot/zzz-reboot.h"
    This reverts commit c097f8cd8450482042beaf6656b8d721f3883136.

--- packages/confine/confine-system/files/etc/firstboot/zzz-reboot.sh
+++ packages/confine/confine-system/files/etc/firstboot/zzz-reboot.sh
@@ -2,8 +2,7 @@
 echo "Firstboot executed, rebooting system in 5 seconds"
 sleep 5
 echo "$(date) zzz-reboot" >> /root/confine.daemon-starts
-sync
-reboot -f
+reboot
 echo "$(date) zzz-reboot-000" >> /root/confine.daemon-starts
 sleep 5
 echo "$(date) zzz-reboot-005" >> /root/confine.daemon-starts
