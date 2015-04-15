44dcff1de321776725429cb88037207c240e0a84
Axel Neumann <neumann@cgws.de>
2015-04-15 14:27:13 +0000

    [confine-parted] etc/uci-defaults/00.-confine-disk-parted.sh: reboot hard if reboot fails

--- packages/confine/confine-parted/files/etc/uci-defaults/00.-confine-disk-parted.sh
+++ packages/confine/confine-parted/files/etc/uci-defaults/00.-confine-disk-parted.sh
@@ -2,5 +2,9 @@
 if confine.disk-parted >> /root/confine.disk-parted.log 2>&1; then
 	reboot
 	# stop this script until reboot is finished
-	sleep 300
+	sleep 5
+	# due to /etc/init.d/boot logic, this script will NOT be removed 
+	# before the one after the next reboot where confine.disk-parted 
+	# returns false. But we dont want more uci-defaults scripts now!
+	reboot -f
 fi
